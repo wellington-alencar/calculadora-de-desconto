28997a28306452baa174b8b8fa1c83ff26ac6138
wellington-alencar <130476068+wellington-alencar@users.noreply.github.com>
2023-09-06 16:54:15 -0300

    Update script.js

--- script.js
+++ script.js
@@ -7,78 +7,97 @@ function Calcular (n1){
     switch(desconto){
         case '5':
             calculo = Math.round((n1 * 0.05))
+            calculo = n1 - calculo
             document.getElementById("resultado").innerHTML = `O valor total com desconto de 5% é ${calculo}`
             break
         case '10':
             calculo = Math.round((n1 * 0.1))
+            calculo = n1 - calculo
             document.getElementById("resultado").innerHTML= `O valor total com desconto de 10% é ${calculo}`
             break
         case '15':
             calculo =Math.round(( n1 * 0.15))
+            calculo = n1 - calculo
             document.getElementById("resultado").innerHTML = `O valor total com desconto de 15% é ${calculo}`
             break
         case '20':
             calculo = Math.round((n1 * 0.2))
+            calculo = n1 - calculo
             document.getElementById("resultado").innerHTML = `O valor total com desconto de 20% é ${calculo}`
             break        
         case '25':
             calculo = Math.round((n1 * 0.25))
+            calculo = n1 - calculo
             document.getElementById("resultado").innerHTML = `O valor total com desconto de 25% é ${calculo}`
             break         
         case '30':
             calculo = Math.round((n1 * 0.3))
+            calculo = n1 - calculo
             document.getElementById("resultado").innerHTML = `O valor total com desconto de 30% é ${calculo}`
             break
         case '35':
             calculo = Math.round((n1 * 0.35))
+            calculo = n1 - calculo
             document.getElementById("resultado").innerHTML = `O valor total com desconto de 35% é ${calculo}`
             break
         case '40':
             calculo =Math.round((n1 * 0.4))
+            calculo = n1 - calculo
             document.getElementById("resultado").innerHTML = `O valor total com desconto de 40% é ${calculo}`
             break        
         case '45':
             calculo = Math.round((n1 * 0.45))
+            calculo = n1 - calculo
             document.getElementById("resultado").innerHTML = `O valor total com desconto de 45% é ${calculo}`
             break        
         case '50':
             calculo = Math.round((n1 * 0.5))
+            calculo = n1 - calculo
             document.getElementById("resultado").innerHTML = `O valor total com desconto de 50% é ${calculo}`
             break         
         case '55':
             calculo = Math.round((n1 * 0.55))
+            calculo = n1 - calculo
             document.getElementById("resultado").innerHTML = `O valor total com desconto de 55% é ${calculo}`
             break        
         case '60':
             calculo = Math.round((n1 * 0.6))
+            calculo = n1 - calculo
             document.getElementById("resultado").innerHTML = `O valor total com desconto de 60% é ${calculo}`
             break         
         case '65':
             calculo = Math.round((n1 * 0.65))
+            calculo = n1 - calculo
             document.getElementById("resultado").innerHTML = `O valor total com desconto de 65% é ${calculo}`
             break         
         case '70':
             calculo = Math.round((n1 * 0.7))
+            calculo = n1 - calculo
             document.getElementById("resultado").innerHTML = `O valor total com desconto de 70% é ${calculo}`
             break        
         case '75':
             calculo = Math.round((n1 * 0.75))
+            calculo = n1 - calculo
             document.getElementById("resultado").innerHTML = `O valor total com desconto de 75% é ${calculo}`
             break         
         case '80':
             calculo = Math.round((n1 * 0.8))
+            calculo = n1 - calculo
             document.getElementById("resultado").innerHTML = `O valor total com desconto de 80% é ${calculo}`
             break 
         case '85':
             calculo = Math.round((n1 * 0.85))
+            calculo = n1 - calculo
             document.getElementById("resultado").innerHTML = `O valor total com desconto de 85% é ${calculo}`
             break 
         case '90':
             calculo = Math.round((n1 * 0.9))
+            calculo = n1 - calculo
             document.getElementById("resultado").innerHTML = `O valor total com desconto de 90% é ${calculo}`
             break         
         case '95':
             calculo = Math.round((n1 * 0.95))
+            calculo = n1 - calculo
             document.getElementById("resultado").innerHTML = `O valor total com desconto de 95% é ${calculo}`
             break
         
